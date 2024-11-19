e2d9cb7fc2a63391cd9461b844b9683109db3c75
July Cardoso <julycardosopereira@gmail.com>
2024-11-19 13:52:38 -0300

    .

--- scripts.js
+++ scripts.js
@@ -33,3 +33,33 @@ setInterval(() => {
 
 // Initial load
 showSlide(0);
+
+
+/*let burguer = true;
+const menuContent = document.querySelector ('.content');
+const menuToggle = menuContent.querySelector ('.menu-toggle');
+
+menuToggle.addEventListener('click',() => {
+    document.body.style.overflow = show? 'hidden' : 'initial'
+   
+    menuContent.classList.toggle('on', show);
+    show = !show;
+}) */
+
+
+function mudaoTamanho() {
+    if(window.innerWidth >= 768) {
+        itens.style.display = 'block'
+     } else{
+        itens.style.display ='none'
+     }
+    }
+ 
+     function clickMenu() {
+    if(itens.style.display == 'block'){ 
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+ 
+ }
