ae0d44bffa5eec1a7c509025027ba8c4c22a88a6
July Cardoso <julycardosopereira@gmail.com>
2024-11-21 19:49:33 -0300

    .

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var itens = document.getElementById('itens');
 
 function mudaoTamanho() {
     if (window.innerWidth >= 768) {
-        itens.style.display = 'block';  // Exibe o menu em telas grandes
+        itens.style.display = 'none';  // Exibe o menu em telas grandes
     } else {
         itens.style.display = 'none';   // Esconde o menu em telas pequenas
     }
@@ -26,81 +26,3 @@ window.addEventListener('resize', function() {
 window.onload = function() {
     mudaoTamanho();  // Ajusta o estado do menu no carregamento da página
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const slides = document.querySelectorAll('.slide');
-    
-    slides.forEach((slide) => {
-        let currentIndex = 0;
-        const images = slide.querySelectorAll('img');
-        const nextButton = slide.querySelector('.next');
-        const prevButton = slide.querySelector('.prev');
-        const intervalTime = 3000;
-
-        // Função para mostrar a imagem atual
-        function showSlide(index) {
-            images.forEach((img, i) => {
-                img.style.opacity = (i === index) ? '1' : '0';
-            });
-        }
-
-        // Navegação para o próximo slide
-        nextButton.addEventListener('click', () => {
-            currentIndex = (currentIndex + 1) % images.length;
-            showSlide(currentIndex);
-        });
-
-        // Navegação para o slide anterior
-        prevButton.addEventListener('click', () => {
-            currentIndex = (currentIndex - 1 + images.length) % images.length;
-            showSlide(currentIndex);
-        });
-
-        // Slideshow automático
-        setInterval(() => {
-            currentIndex = (currentIndex + 1) % images.length;
-            showSlide(currentIndex);
-        }, intervalTime);
-
-        // Exibe o slide inicial
-        showSlide(currentIndex);
-    });
-
-
-
-});
-
-
-
-const myObserver = new IntersectionObserver((entries)=>{
-entries.forEach(  (entry) =>{
-    if(entry.isIntersecting){
-        entry.target.classList.add('show')
-    }
-   
-    else{
-        entry.target.classList.remove('show')
-    }
-
-
-})
-
-}) 
-
-const elements = document.querySelectorAll('.hidden')
-
-
-
-elements.forEach((element)  => myObserver.observe(element)) 
